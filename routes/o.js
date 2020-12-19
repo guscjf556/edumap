b1ad04e14dab2fced2bcbcf4315b2ae5bb5860ff
Marcelim <guscjf556@gmail.com>
2020-12-19 23:00:22 +0900

    인피니트 스크롤 기능 구현, 포맷 정리(의도치 않은...)

--- routes/o.js
+++ routes/o.js
@@ -1,38 +1,40 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var bodyParser = require('body-parser');
-var multer  = require('multer')
-var template = require('../lib/template.js');
-var fs = require('fs');
-var db = require('../lib/db');
-const mapMaker = require('../lib/mapMaker');
+var bodyParser = require("body-parser");
+var multer = require("multer");
+var template = require("../lib/template.js");
+var fs = require("fs");
+var db = require("../lib/db");
+const mapMaker = require("../lib/mapMaker");
 
 //멀터 설정 어디에 사진파일을 저장할지
 var _storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'public/images/')
+    cb(null, "public/images/");
   },
   filename: function (req, file, cb) {
-    cb(null,Date.now() + '_' + file.originalname)
-  }
-})
-var upload = multer({ storage: _storage })
-
+    cb(null, Date.now() + "_" + file.originalname);
+  },
+});
+var upload = multer({ storage: _storage });
 
 // 기본템플릿
-var template = require('../lib/template.js');
-var auth = require('../lib/auth');
+var template = require("../lib/template.js");
+var auth = require("../lib/auth");
 
 //개인별 데이터
-router.get('/user', (req, res) => {
-  if(!auth.IsOwner(req,res)){
-    res.redirect('/o/notLogin');
+router.get("/user", (req, res) => {
+  if (!auth.IsOwner(req, res)) {
+    res.redirect("/o/notLogin");
     return false;
   }
-  db.query('SELECT * FROM topic WHERE topic.user_id = ? ',[req.user.id], function(err, result){
-    if(err)throw err;
-    function cardList(id,title,description,imagePath){
-      return `
+  db.query(
+    "SELECT * FROM topic WHERE topic.user_id = ? ",
+    [req.user.id],
+    function (err, result) {
+      if (err) throw err;
+      function cardList(id, title, description, imagePath) {
+        return `
           <div>
             <a href="/o/${id}" class="text-decoration-none">
               <div class="card border-0 rounded-lg">
@@ -45,112 +47,143 @@ router.get('/user', (req, res) => {
             </a>
           </div>
     `;
+      }
+      var card_list = '<div class="card-columns my-3">';
+      var i = 0;
+      while (i < result.length) {
+        var o_id = result[i].id;
+        var imagePath = result[i].o_image_1;
+        var card_o_name = result[i].o_name;
+        var description = result[i].description;
+        card_list =
+          card_list + cardList(o_id, card_o_name, description, imagePath);
+        i = i + 1;
+      }
+      var card_list = card_list + "</div>";
+      var html = template.HTML(card_list, auth.StatusUI(req, res));
+      res.send(html);
     }
-  var card_list = '<div class="card-columns my-3">';
-    var i = 0;
-    while(i < result.length){
-      var o_id = result[i].id;
-      var imagePath =result[i].o_image_1;
-      var card_o_name = result[i].o_name;
-      var description = result[i].description;
-      card_list = card_list + cardList(o_id, card_o_name,description,imagePath);
-      i = i + 1;
-    }
-    var card_list = card_list + '</div>';
-    var html = template.HTML(card_list, auth.StatusUI(req, res));
-    res.send(html);
-});
+  );
 });
 
 //로그인 안되었을시
-router.get('/notLogin', (req, res)=> {
-  const html = template.HTML(`
+router.get("/notLogin", (req, res) => {
+  const html = template.HTML(
+    `
   로그인을 해야 이용할 수 있습니다.<br>
   <a href = "/u/login">로그인하기</a><br>
   <a href = "/o">홈으로 돌아가기</a>
-  `, auth.StatusUI(req, res));
-  res.send(html);
-})
-
-// 카드 및 첫화면
-router.get('/', (req, res) => {
-    db.query('SELECT * FROM topic', function(err, result){
-      function cardList(id,title,description,imagePath){
-          return `
-              <div>
-                <a href="/o/${id}" class="text-decoration-none">
-                  <div class="card border-0 rounded-lg">
-                    <img src="../${imagePath}" class="card-img-top w-100" alt="card image cap">
-                      <div class="card-body">
-                        <h5 class="card-title text-dark font-weight-bolder">${title}</h5>
-                        <p class="card-text text-dark">${description}</p>
-                      </div>
-                  </div>
-                </a>
-              </div>
-        `;
-        }
-      var card_list = '<div class="card-columns my-3">';
-        var i = 0;
-        while(i < result.length){
-          var o_id = result[i].id;
-          var imagePath =result[i].o_image_1;
-          var card_o_name = result[i].o_name;
-          var description = result[i].description;
-          card_list = card_list + cardList(o_id, card_o_name,description,imagePath);
-          i = i + 1;
-        }
-        var card_list = card_list + '</div>';
-        var html = template.HTML(card_list, auth.StatusUI(req, res));
-        res.send(html);
-    });
-  });
-
-
-  //생성
-router.get('/create', (req,res)=>{
-  if(!auth.IsOwner(req,res)){
-    res.redirect('/o');
-    return false;
-  }
-  const html = template.HTML(template.create(),auth.StatusUI(req, res));
+  `,
+    auth.StatusUI(req, res)
+  );
   res.send(html);
 });
 
+// 카드 및 첫화면
+router.get("/", (req, res) => {
+  db.query("SELECT * FROM topic ORDER BY id DESC", function (err, result) {
+    const result_string = JSON.stringify(result);
+    const card_list = `
+    <div class="container" style = "margin-top:20px">
+      <div id="card-cols" class="card-columns my-3">
+      </div>
+    </div>
+    <script>
+      const postData = ${result_string};
+      let postCounter = 10
+      function cardList(id, title, description, imagePath) {
+        return '<div><a href="/o/' + id + '" class="text-decoration-none"><div class="card border-0 rounded-lg"><img src="../' + imagePath + '" class="card-img-top w-100" alt="card image cap"><div class="card-body"><h5 class="card-title text-dark font-weight-bolder">' + title + '</h5><p class="card-text text-dark">' + description + '</p></div></div></a></div>';
+      };
+
+      var card_list = '';
+      var i = 0;
+      while (i < postCounter && postData[i]) {
+        var o_id = postData[i].id;
+        var imagePath = postData[i].o_image_1;
+        var card_o_name = postData[i].o_name;
+        var description = postData[i].description;
+        card_list = card_list + cardList(o_id, card_o_name, description, imagePath);
+        i = i + 1;
+      }
+      document.getElementById("card-cols").innerHTML = card_list;
+      window.onscroll = function(e){
+        if(window.scrollY + document.documentElement.clientHeight === document.documentElement.scrollHeight){
+          for(var i = postCounter; i < postCounter + 10; i++){
+            if(postData[i]){
+              var o_id = postData[i].id;
+              var imagePath = postData[i].o_image_1;
+              var card_o_name = postData[i].o_name;
+              var description = postData[i].description;
+              card_list = card_list + cardList(o_id, card_o_name, description, imagePath);
+            }
+          }
+        document.getElementById("card-cols").innerHTML = card_list;
+        postCounter += 10;
+        }
+      }
+    </script>
+    `;
+    var html = template.HTML(card_list, auth.StatusUI(req, res));
+    res.send(html);
+  });
+});
+
+//생성
+router.get("/create", (req, res) => {
+  if (!auth.IsOwner(req, res)) {
+    res.redirect("/o");
+    return false;
+  }
+  const html = template.HTML(template.create(), auth.StatusUI(req, res));
+  res.send(html);
+});
 
 //생성_과정
 let image_array = upload.fields([
-  { name: 'o_image_1', maxCount: 1},
-  { name: 'o_image_2', maxCount: 1},
-  { name: 'o_image_3', maxCount: 1},
-  { name: 'o_image_4', maxCount: 1},
-  { name: 'o_image_5', maxCount: 1},
-])
-router.post('/create_process', image_array,function (req, res, next) {
-  var images = []
-  for(var i = 1; i < 6; i++){
-    if (req.files[`o_image_${i}`] == undefined){
+  { name: "o_image_1", maxCount: 1 },
+  { name: "o_image_2", maxCount: 1 },
+  { name: "o_image_3", maxCount: 1 },
+  { name: "o_image_4", maxCount: 1 },
+  { name: "o_image_5", maxCount: 1 },
+]);
+router.post("/create_process", image_array, function (req, res, next) {
+  var images = [];
+  for (var i = 1; i < 6; i++) {
+    if (req.files[`o_image_${i}`] == undefined) {
       images.push(null);
-    }
-    else{
-      images.push(req.files[`o_image_${i}`][0].path.slice(7))
+    } else {
+      images.push(req.files[`o_image_${i}`][0].path.slice(7));
     }
   }
-  db.query('INSERT INTO topic (o_name, description, created, o_image_1, o_image_2, o_image_3, o_image_4, o_image_5, user_id, Lat, Lng) VALUES (?, ?, ?, ?, ?, ? , ? , ?, ?, ?, ?)',
-  [req.body.o_name, req.body.o_memo, req.body.o_time, images[0], images[1], images[2], images[3], images[4], req.user.id,  req.body.Lat, req.body.Lng],function(err, result){
-    if(err)throw err;
-  })
+  db.query(
+    "INSERT INTO topic (o_name, description, created, o_image_1, o_image_2, o_image_3, o_image_4, o_image_5, user_id, Lat, Lng) VALUES (?, ?, ?, ?, ?, ? , ? , ?, ?, ?, ?)",
+    [
+      req.body.o_name,
+      req.body.o_memo,
+      req.body.o_time,
+      images[0],
+      images[1],
+      images[2],
+      images[3],
+      images[4],
+      req.user.id,
+      req.body.Lat,
+      req.body.Lng,
+    ],
+    function (err, result) {
+      if (err) throw err;
+    }
+  );
 
-  res.redirect('/o');
-})
-
-
+  res.redirect("/o");
+});
 
 //  글 수정하기
-router.post('/update/:pageId', (req, res) => {
+router.post("/update/:pageId", (req, res) => {
   var pageId = req.params.pageId;
-  db.query('SELECT * FROM topic WHERE id=?', [pageId],function(err, result){
-      var html =template.HTML(`
+  db.query("SELECT * FROM topic WHERE id=?", [pageId], function (err, result) {
+    var html = template.HTML(
+      `
       <form action = "/o/update_process" method = "post">
       <div class=search_filter>
         <input type="hidden" name="topic_id" value="${result[0].id}">
@@ -186,73 +219,95 @@ router.post('/update/:pageId', (req, res) => {
         <input type = "submit" value ="삭제하기">
       </form>
 
-      `,auth.StatusUI(req, res)
-      );
-      res.send(html);
-    });
-  })
-
-  // 글 수정하기 process
-  router.post('/update_process', (req, res) => {
-    var post = req.body;
-    var o_name = post.o_name;
-    var created = post.created;
-    var description = post.description;
-    var topic_id = post.topic_id
-    db.query('UPDATE topic SET o_name = ? , description = ?,  created = ? WHERE id = ?', [o_name, description, created, topic_id], (err, result) =>{
-      if(err)throw err;
-      res.redirect(`/o/${topic_id}`);
-    })
+      `,
+      auth.StatusUI(req, res)
+    );
+    res.send(html);
   });
-
-// 글 삭제하기
-router.post('/delete', (req, res) => {
-  db.query('SELECT * FROM topic WHERE id = ?',[req.body.o_id],function(err, result){
-    if(err)throw err;
-    var imageArray = Object.values(result[0])
-    for(var i = 4; i < 9; i ++){
-      if(imageArray[i] === null){
-        break;
-      }
-      fs.unlinkSync(`public/${imageArray[i]}`)
-      }
-    db.query('DELETE FROM topic WHERE id = ?',[req.body.o_id]),function(err, result){
-      if(err)throw err;
-    }
-  });
-    res.redirect('/o');
 });
 
+// 글 수정하기 process
+router.post("/update_process", (req, res) => {
+  var post = req.body;
+  var o_name = post.o_name;
+  var created = post.created;
+  var description = post.description;
+  var topic_id = post.topic_id;
+  db.query(
+    "UPDATE topic SET o_name = ? , description = ?,  created = ? WHERE id = ?",
+    [o_name, description, created, topic_id],
+    (err, result) => {
+      if (err) throw err;
+      res.redirect(`/o/${topic_id}`);
+    }
+  );
+});
+
+// 글 삭제하기
+router.post("/delete", (req, res) => {
+  db.query(
+    "SELECT * FROM topic WHERE id = ?",
+    [req.body.o_id],
+    function (err, result) {
+      if (err) throw err;
+      var imageArray = Object.values(result[0]);
+      for (var i = 4; i < 9; i++) {
+        if (imageArray[i] === null) {
+          break;
+        }
+        fs.unlinkSync(`public/${imageArray[i]}`);
+      }
+      db.query("DELETE FROM topic WHERE id = ?", [req.body.o_id]),
+        function (err, result) {
+          if (err) throw err;
+        };
+    }
+  );
+  res.redirect("/o");
+});
 
 // 상세보기
-router.get('/:pageId' , (req, res) => {
+router.get("/:pageId", (req, res) => {
   var pageId = req.params.pageId;
-  db.query('SELECT * FROM topic LEFT JOIN user ON topic.user_id = user.id WHERE topic.id = ?',[pageId],function(err, result){
-    var str = ""
-    var carouselIndicators=""
-    var imageNum = Object.keys(result[0]).length ;
-    var imageArray = Object.values(result[0])
-    var carouselIndicatorsHowMany = 1;
-    for(var i = 5; i < 9; i ++){
-      if(imageArray[i] === null){
-        continue;
+  db.query(
+    "SELECT * FROM topic LEFT JOIN user ON topic.user_id = user.id WHERE topic.id = ?",
+    [pageId],
+    function (err, result) {
+      var str = "";
+      var carouselIndicators = "";
+      var imageNum = Object.keys(result[0]).length;
+      var imageArray = Object.values(result[0]);
+      var carouselIndicatorsHowMany = 1;
+      for (var i = 5; i < 9; i++) {
+        if (imageArray[i] === null) {
+          continue;
+        } else {
+          carouselIndicators =
+            carouselIndicators +
+            `<li data-target="#carouselPost" data-slide-to="${carouselIndicatorsHowMany}"></li>`;
+          carouselIndicatorsHowMany += 1;
+          var str =
+            str +
+            '<div class="carousel-item"' +
+            ">" +
+            '<img src="../' +
+            imageArray[i] +
+            '" class="d-block w-100" alt="' +
+            i +
+            '"' +
+            ">" +
+            `</div>`;
+        }
       }
-      else {
-        carouselIndicators = carouselIndicators + `<li data-target="#carouselPost" data-slide-to="${carouselIndicatorsHowMany}"></li>`
-        carouselIndicatorsHowMany += 1;
-        var str = str + '<div class="carousel-item"' + '>' +
-        '<img src="../'+ imageArray[i]  +'" class="d-block w-100" alt="'+ i + '"' + '>' + 
-        `</div>`
+      var date = result[0].created.toLocaleDateString("ko-KR");
+      var koreanDate = ["년 ", "월 "];
+      for (var i = 0; i < 2; i++) {
+        date = date.replace("-", koreanDate[i]);
       }
-    }
-    var date = result[0].created.toLocaleDateString('ko-KR');
-    var koreanDate = ["년 ", "월 "];
-    for(var i = 0; i < 2; i++){
-      date = date.replace('-', koreanDate[i]);
-    }
-    date = date + "일";
-    const LatLng = `${result[0].Lat},${result[0].Lng}`
-      var html = template.HTML(`
+      date = date + "일";
+      const LatLng = `${result[0].Lat},${result[0].Lng}`;
+      var html = template.HTML(
+        `
       <h1>${result[0].o_name}</h1>
       <div class="row row-cols-1 row-cols-md-2">
         <div class = "col">
@@ -263,7 +318,9 @@ router.get('/:pageId' , (req, res) => {
           </ol>
             <div class="carousel-inner">
             <div class="carousel-item active"  >
-              <img src="../${result[0].o_image_1}" class="d-block w-100" alt="1">
+              <img src="../${
+                result[0].o_image_1
+              }" class="d-block w-100" alt="1">
             </div>
             ${str}
           </div>
@@ -278,15 +335,22 @@ router.get('/:pageId' , (req, res) => {
           </div>
         </div>
         <div class ="col">
-          <h4>${result[0].displayName} <small class="text-muted">${result[0].description}</small></h4>
+          <h4>${result[0].displayName} <small class="text-muted">${
+          result[0].description
+        }</small></h4>
           <p>${date}</p>
-          ${mapMaker.move('width:20rem;height:20rem;', 3 ,`${LatLng}`,`{
+          ${mapMaker.move(
+            "width:20rem;height:20rem;",
+            3,
+            `${LatLng}`,
+            `{
             content: '<div><a href="/o/${result[0].id}" target = "_blank">${result[0].o_name}</a></div>', 
             latlng: new kakao.maps.LatLng(${result[0].Lat}, ${result[0].Lng})
-        }`)}
+        }`
+          )}
           <form action = "/o/update/${pageId}" method = "post">
             <input type = "hidden" name = "o_id"  value = "${pageId}">
-            <input type = "${auth.updateHide(req,result)}" value ="수정하기">
+            <input type = "${auth.updateHide(req, result)}" value ="수정하기">
           </form>
         </div>
       </div>
@@ -294,10 +358,13 @@ router.get('/:pageId' , (req, res) => {
        if(${result[0].Lat}===0){
         document.querySelector('#map').style.display="none";
        }
-      </script>
-    `,auth.StatusUI(req, res));
-    res.send(html);
-    });
-  });
+      </>
+    `,
+        auth.StatusUI(req, res)
+      );
+      res.send(html);
+    }
+  );
+});
 
-  module.exports = router;
+module.exports = router;
