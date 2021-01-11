4b53ca670fee7f71a4418119484f64f5de525419
Marcelim <guscjf556@gmail.com>
2021-01-12 04:51:20 +0900

    메인 뉴스피드 렌더

---
+++ components/newsFeed.js
@@ -0,0 +1,53 @@
+const newsFeed = (dbQueryResult) => {
+  const result_string = JSON.stringify(dbQueryResult);
+  const  render = `
+  <div class="my-3">
+    <div id="card-cols" class="card-columns my-3">
+    </div>
+    <button class="btn btn-outline-dark d-block mx-auto" onclick="loadMore();">더보기</button>
+  </div>
+  <!-- 관찰 올리기 버튼 -->
+    <svg xmlns="http://www.w3.org/2000/svg" width="2rem" height="2rem" fill="currentColor" class="bi bi-plus text-light bg-success rounded-circle shadow-sm d-block ml-auto mr-3 mb-3 fixed-bottom d-lg-none" viewBox="0 0 16 16" onclick="showModal()">
+    <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/></svg>
+  <script>
+    //브라우저에서 데이터를 조작할 수 있도록 postData 변수 생성
+    //예상되는 문제: 데이터가 많아지면 불러와서 변수에 담는 데 시간 소요, 일단 100개만 select하고 필요할 때 새롭게 100개를 받아오는 방식으로 수정 필요할 듯 
+    const postData = ${result_string};
+    let postCounter = 10
+    function cardList(id, title, description, imagePath) {
+      return '<a href="/o/' + id + '" class="text-decoration-none"><div class="card border-0 rounded-lg shadow-sm"><img src="../' + imagePath + '" class="card-img-top w-100 img-thumbnail" alt="card image cap"><div class="card-body"><h5 class="card-title text-dark font-weight-bolder">' + title + '</h5><p class="card-text text-dark">' + description + '</p></div></div></a>';
+    };
+    //첫 게시물 일단 10개만 뜨도록(개수는 postCounter로 수정 가능)
+    var card_list = '';
+    var i = 0;
+    while (i < postCounter && postData[i]) {
+      var o_id = postData[i].id;
+      var imagePath = postData[i].o_image_1;
+      var card_o_name = postData[i].o_name;
+      var description = postData[i].description;
+      card_list = card_list + cardList(o_id, card_o_name, description, imagePath);
+      i = i + 1;
+    }
+    document.getElementById("card-cols").innerHTML = card_list;
+
+    //무한스크롤(스크롤 끝까지 내리면 10개씩 새로 뜨도록)
+    function loadMore(){
+      for(var i = postCounter; i < postCounter + 10; i++){
+        if(postData[i]){
+          var o_id = postData[i].id;
+          var imagePath = postData[i].o_image_1;
+          var card_o_name = postData[i].o_name;
+          var description = postData[i].description;
+          card_list = card_list + cardList(o_id, card_o_name, description, imagePath);
+        }
+      }
+    document.getElementById("card-cols").innerHTML = card_list;
+    postCounter += 10;
+    }
+  </script>
+  `;
+
+  return render
+}
+
+module.exports = newsFeed;
