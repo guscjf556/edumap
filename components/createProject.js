8f963842d5421a142a7b7490f584b4ba33656990
marcelim <guscjf556@gmail.com>
2021-01-31 22:32:49 +0900

    중복체크 자동화, 허용돼야 프로젝트 생성 가능하게

--- components/createProject.js
+++ components/createProject.js
@@ -6,7 +6,7 @@ const render =`
     <div class="card" style="width: 80%;margin: auto">
     <div class="card-body">
       <div class="card-text">
-        <form id="createProjectForm" action = "/o/create-project-process" method = "post">
+        <form id="createProjectForm" action = "/o/create-project-process" method = "post" onsubmit="return didPassCheck();">
           <div class="form-group">
             <label for="projectTitle">프로젝트 이름</label>
             <input type="text" class="form-control" id="projectTitle" name="projectTitle" pattern="^.+$" required>
@@ -19,32 +19,54 @@ const render =`
           <div class="form-group">
             <label for="projectPasscode">프로젝트 가입 코드</label>
             <input type="text" class="form-control" id="projectPasscode" name="projectPasscode" required pattern="^\\S{8,}$">
-            <button type="button" id="duplicateCheck" class="btn btn-sm btn-warning" onclick=(checkDuplicatePasscode())>중복확인<small class="badge badge-pill badge-dark">필수</small></button>
+            <!--
+            <button type="button" id="duplicateCheck" class="btn btn-sm btn-warning" onclick="checkDuplicatePasscode()">중복확인<small class="badge badge-pill badge-dark">필수</small></button>
+            -->
             <small id="duplicateCheckResult" class="form-text text-muted"></small>
-            <small class="form-text text-muted">다른 사용자의 프로젝트 가입 시 필요한 코드입니다. 8개 이상의 숫자, 문자를 사용해주세요. (이 코드는 암호화되지 않습니다. 개인적인 비밀번호를 사용하지 말아주세요.)</small>
+            <small class="form-text text-muted">다른 사용자의 프로젝트 가입 시 필요한 코드입니다. 8개 이상의 문자(숫자, 특수기호 사용 가능)를 사용해주세요. (이 코드는 암호화되지 않습니다. 개인적인 비밀번호를 사용하지 말아주세요.)</small>
           </div>
-          <input type="submit" class="btn btn-success w-100" value="프로젝트 만들기">
+          <input id="createProjectSubmit" type="submit" class="btn btn-success w-100" value="프로젝트 만들기" disabled>
         </form>
       </div>
     </div> 
   </div>
   <script>
-  const checkDuplicatePasscode = () => {
-    const projectPasscode = $('#projectPasscode').val()
+  let didCheck = false;
+  $('#projectPasscode').change((e) => {
+    console.log(e.target);
+    if(e.target.value.length < 8){
+      $('#duplicateCheckResult').html('<span class="text-danger">8개 이상의 문자를 사용해야합니다(숫자, 특수기호 사용 가능)</span>');
+      didCheck = false;
+      return;
+    }
     $.ajax({
       url: '/o/check-duplicate-passcode',
       type: 'post',
-      data: { passcode: projectPasscode },
+      data: { passcode: e.target.value },
       success: function(res){
         if(!res){
           $('#duplicateCheckResult').html('<span class="text-success">사용 가능한 코드입니다.</span>');
+          didCheck = true;
+          $('#createProjectSubmit').prop("disabled", false);
         }
         else {
-          $('#duplicateCheckResult').html('<span class="text-danger">다른 코드를 사용해 주세요.</span>');
+          $('#duplicateCheckResult').html('<span class="text-danger">허용되지 않은 코드입니다. 다른 코드를 사용해 주세요.</span>');
+          didCheck = false;
+          $('#createProjectSubmit').prop("disabled", true);
         }
       },
     });
-  }
+  });
+
+  const didPassCheck = () => {
+    if(didCheck){
+      return true;
+    }
+    else {
+      return false;
+    };
+  };
+
   </script>
 `
 
